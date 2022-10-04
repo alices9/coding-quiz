bf1121ffcd2f6653812c52e777589f01005ad007
Alice Shao <a1ice_shao@yahoo.com>
2022-10-03 20:41:47 -0400

    created the display function in javascript to display the questions

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
-timerEl = document.getElementById("countdown");
-starterEl = document.getElementById("starter");
+var timerEl = document.getElementById("countdown");
+var starterEl = document.getElementById("starter");
+var mainTextEl = document.getElementById("main-text");
+var introEl = document.getElementById("intro");
+var choiceAEl = document.getElementById("choice-a")
+var choiceBEl = document.getElementById("choice-b")
+var choiceCEl = document.getElementById("choice-c")
+var choiceDEl = document.getElementById("choice-d")
+
+var questionList = ["How do you get the length of an array?"];
+var answerListA = [".length"];
+var answerListB =[".length - 1"];
+var answerListC =[".length + 1"];
+var answerListD =[".count"];
+
+
+
+
 
 starterEl.addEventListener("click", function(){
     countdown();
     starterEl.setAttribute("visibility", "hidden");
+    introEl.textContent = "";
+    display();
 })
 
 function countdown() {
@@ -19,3 +37,18 @@ function countdown() {
         }
     }, 1000);
 }
+
+function display() {
+    for (var i = 0; i <=9; i++){
+        if (timerEl !== 0){
+
+            mainTextEl.textContent = questionList[i];
+            choiceAEl.textContent = answerListA[i];
+            choiceBEl.textContent = answerListB[i];
+            choiceCEl.textContent = answerListC[i];
+            choiceDEl.textContent = answerListD[i];
+        }
+        return;
+    }
+}
+    
